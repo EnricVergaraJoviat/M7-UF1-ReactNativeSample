0979920be5d2bf9d78521e518fa8a5bf1502bc4c
Enric Vergara <donkikochan@gmail.com>
2024-01-09 12:49:17 +0100

    finished panel

--- components/OurPanel.js
+++ components/OurPanel.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
 
-function OurPanel({title, subtitle}) {
+function OurPanel({title, subtitle, function1, function2}) {
     
     return (
         <View style={{backgroundColor: "#FF00F1", width: '80%'}}>
             <Text>{title}</Text>
             <Text>{subtitle}</Text>
+            <Button
+                title="Action1"
+                onPress={function1}
+            />
+            <Button
+                title="Action2_1"
+                onPress={()=>function2("Acció 2.1")}
+            />
+            <Button
+                title="Action2_2"
+                onPress={()=>function2("Acció 2.2")}
+            />
         </View>
     );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,13 +5,24 @@ import OurPanel from '../components/OurPanel';
 
 function HomeScreen({ navigation }) {
     const [text, setText] = useState('');
+    const [info, setInfo] = useState('');
     const [info2, setInfo2] = useState(2);
+    const [showPanel, setShowPanel] = useState(true);
 
+    const myFunction1 = () =>{
+        console.log("myFunction1");
+        setShowPanel(false);
+    }
+
+    const myFunction2 = (info) =>{
+        console.log("myFunction2: " + info);
+        setInfo("myFunction2: " + info);
+    }
     return (
     <View>
-        <OurPanel title='HOla' subtitle= 'Adeu'/>
+        {showPanel &&<OurPanel title='HOla' subtitle= 'Adeu' function1={myFunction1} function2={myFunction2}/>}
         
-        <Text>Home Screen</Text>
+        <Text>{info}</Text>
        
         <TextInput
         style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '80%' }}
