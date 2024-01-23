67762ec3eff42bd0eeafa10801ce242b22a5b913
Enric Vergara <donkikochan@gmail.com>
2024-01-23 12:59:33 +0100

    ...

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text, StyleSheet, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import CrosshairImage from '../assets/crosshair.png'
 
 
 function DetailsScreen({route, navigation}) {
@@ -17,20 +18,39 @@ function DetailsScreen({route, navigation}) {
           setMarkerPosition({ latitude: camera.center.latitude, longitude: camera.center.longitude });
     
         }
-      };
+    };
+    
+    const check = () => {
+      console.log("Check");
+    }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             
-            <Button
-            title="Set Mark"
-            onPress={setMark}
-            />
-             <MapView ref={mapRef} style={styles.map} mapType='satellite'>
-                {markerPosition && (
-                <Marker coordinate={markerPosition} title="Marcador" description="Este es el centro del mapa" />
-                )}
-            </MapView>
+            <View style={{flexDirection:'row'}}>
+              <Button
+                title="Set Mark"
+                onPress={setMark}
+              />
+              <Button
+                title="Check"
+                onPress={check}
+              />
+            </View>
+            <View style={styles.map} >
+              <MapView ref={mapRef}  style={{width:'100%', height:'100%'}}mapType='satellite'>
+                  {markerPosition && (
+                  <Marker coordinate={markerPosition} title="Marcador" description="Este es el centro del mapa" />
+                  )}
+              </MapView>
+              <View pointerEvents="none" style={styles.mapCenterMarkerView}>
+              <Image 
+                  style={{width:'10%', height:'10%'}}
+                  source={CrosshairImage}  
+              />
+              </View>
+            </View>
+             
             <Text>Find Barcelona</Text>
         </View>
         
@@ -45,5 +65,16 @@ function DetailsScreen({route, navigation}) {
     map: {
       width: '80%',
       height: '50%',
+      backgroundColor: 'red',
     },
+    mapCenterMarkerView: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  
   });
